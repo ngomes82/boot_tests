5354bdc5772d861faad9494d6bdde641025c2c91
ngomes82 <nickgomes82@gmail.com>
2017-08-10 16:49:04 -0400

    Update boot.asm
    Added the ability to draw to video memory so we don't have to rely on CPU interrupts for output. Added examples of calls to both (and the underlying logic concerning writing to video mem).

--- boot.asm
+++ boot.asm
@@ -9,15 +9,65 @@
 
 	xor ax, ax		;Clear AX, first boot might be all screwy.
 	mov ds, ax		;Point Data segment to correct mem location (0)
+	mov ss, ax		;Stack starts at 0
+	mov sp, 0x9c00	;20000h past code start
 	mov si, msg		;Move starting address of msg to SI register
 	call biosprintstring 
 	
 	cli				;Disable interrupts, to start entering protected mode
+	cld				;DI is incremented when rep instruction is called
+	
+	;----------------------------------------------
+	;Draw a single red square at 0,0 as an example of video memory drawing
+	mov ax, 0xb800   ; look at video mem
+	mov es, ax		 
+	mov ax,0x4020 	 ;colour + space character(0x20)
+	xor di, di		 ;DI position 0,0
+	stosw			 ;Move contents of AX into Address located in ES
+	;----------------------------------------------
+	
+	
+	mov ax, 0xb800  ;Move to Video Mem
+	mov es, ax
+	
+	mov si, msg
+	call vmemprintstring
+	
 	
 hang:
 	jmp hang			;loop the bootloader forever
 
 
+	
+dochar:   
+	call vmemprintchar         ; print one character
+	
+vmemprintstring:
+	lodsb			;load string char to al
+	cmp al, 0
+	jne dochar
+	add byte[ypos], 1 ;down one row (LF)
+	mov byte[xpos], 0 ;back to left (CR)
+	ret
+	
+vmemprintchar:
+	mov ah, 0xF0      	 ;White on black color
+	mov cx, ax		  	 ;Save char/attribute
+	movzx ax, byte[ypos] ;grab ypos
+	mov dx, 160			 ;2 bytes (char/attrib)
+	mul dx				 ;80 columns
+	movzx bx, byte[xpos] ;grab xpos to bx
+	shl bx, 1			 ;times 2 to skip attrib
+	
+	mov di, 0			 ;start of video mem
+	add di, ax			 ;add yoffset
+	add di, bx			 ;add xoffset
+	
+	mov ax, cx			 ;restore char/attrib
+	stosw				 ;write char/attribute
+	add byte[xpos], 1	 ;advance right
+	ret
+	
 biosprintstring:		;Use BIOS to pring char string in SI register
 	lodsb
 	or al, al
@@ -38,6 +88,8 @@ biosprintchar:			;Use BIOS to print char residing in the AL register
 
 
 ;====== DATA SEGMENT ;============
+xpos db 0
+ypos db 1
 msg	db 'Hello World!', 0	;Declare 0 terminated set of characters 'H', 'E', 'L', 'L', 'O', '0'
 	times 510-($-$$) db 0	;BIOS looks for a 512 byte Boot Sector, subtracting from data above this line, fill up to 510 bytes with 0's
 	dw 0x55AA				;Boot Signature (0x55AA) The last 2 bytes of 512 Boot Sector. Older bios's look for this.
